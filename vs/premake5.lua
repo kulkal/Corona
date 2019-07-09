d1dca51a952930aebc54b9c5ad6c829ab9251ac0
kulkal <kulkal@gmail.com>
2019-07-09 16:08:49 +0900

    .

--- vs/premake5.lua
+++ vs/premake5.lua
@@ -7,6 +7,14 @@ project "dx12_framework"
    language "C"
    targetdir "bin/%{cfg.buildcfg}"
    includedirs { "C:/Program Files (x86)/Windows Kits/10/Include/10.0.17763.0/um" }
+   
+   links{
+      "d3d12",
+      "dxgi",
+      "d3dcompiler",
+      "dxguid",
+      "GFSDK_Aftermath_Lib.x64"
+         }
 
    files { "../src/**.h", "../src/**.cpp" }
 
